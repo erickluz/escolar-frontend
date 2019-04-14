8ecf495beddfe953786de79dac056afa61655ae4
Erick <erickluz360@gmail.com>
2019-04-14 00:16:48 -0300

    Alteracao no template do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,44 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AlunoComponent } from './aluno/form-alunos/aluno.component';
-import { ListagemAlunosComponent } from './aluno/listagem-alunos/listagem-alunos.component';
+
 import { HomeComponent } from './home/home.component';
+import { AulasComponent } from './aulas/aulas.component';
+import { ProfessoresComponent } from './professores/professores.component';
+import { DisciplinasComponent } from './disciplinas/disciplinas.component';
+import { TurmasComponent } from './turmas/turmas.component';
+import { CursosComponent } from './cursos/cursos.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'aluno/cadastro', component: AlunoComponent },
-  { path: 'aluno/listagem', component: ListagemAlunosComponent }
+  {path: '', component: HomeComponent}, 
+  {path: 'alunos', component: AulasComponent},
+  {path: 'professores', component: ProfessoresComponent},
+  {path: 'disciplinas', component: DisciplinasComponent},
+  {path: 'turmas', component: TurmasComponent},
+  {path: 'cursos', component: CursosComponent},
+  {path: 'aulas', component: AulasComponent},
+  {
+    path: 'form',
+    data: {
+      title: 'Form',
+    },
+    children: []
+  }, {
+    path: 'login',
+    loadChildren: './+login/login.module#LoginModule',
+    data: {
+      customLayout: true
+    }
+  }, {
+    path: 'register',
+    loadChildren: './+register/register.module#RegisterModule',
+    data: {
+      customLayout: true
+    }
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+
+import { adminLteConf } from './admin-lte.conf';
+
 import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
+
+import { LayoutModule, AccordionModule } from 'angular-admin-lte';
+
 import { AppComponent } from './app.component';
-import { AlunoComponent } from './aluno/form-alunos/aluno.component';
-import { ListagemAlunosComponent } from './aluno/listagem-alunos/listagem-alunos.component';
 import { HomeComponent } from './home/home.component';
 
+import { LoadingPageModule, MaterialBarModule } from 'angular-loading-page';
+import { CursosComponent } from './cursos/cursos.component';
+import { DisciplinasComponent } from './disciplinas/disciplinas.component';
+import { TurmasComponent } from './turmas/turmas.component';
+import { AulasComponent } from './aulas/aulas.component';
+import { ProfessoresComponent } from './professores/professores.component';
+import { AlunosComponent } from './alunos/alunos.component';
+
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    AlunoComponent,
-    ListagemAlunosComponent,
-    HomeComponent
-  ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule,
+    CoreModule,
+    LayoutModule.forRoot(adminLteConf),
+    LoadingPageModule, MaterialBarModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CursosComponent,
+    DisciplinasComponent,
+    TurmasComponent,
+    AulasComponent,
+    ProfessoresComponent,
+    AlunosComponent
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
