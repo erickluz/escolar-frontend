a0bd8c4b860ab7b34db242ca95d69aa9bde4717c
Erick Luz <erick.luz4@etec.sp.gov.br>
2019-08-09 08:37:06 -0300

    Correcoes

--- src/app/cadastro/aluno/matricula/list-matricula/list-matricula.component.ts
+++ src/app/cadastro/aluno/matricula/list-matricula/list-matricula.component.ts
@@ -2,22 +2,42 @@ import { Component, OnInit } from '@angular/core';
 import { Matricula } from '../../../../shared/matricula.model';
 import { ALUNO, MATRICULA1, MATRICULA2 } from '../../../../sge.mock';
 import { Aluno } from '../../../../shared/aluno.model';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EscolarService } from '../../../escolar.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-list-matricula',
   templateUrl: './list-matricula.component.html',
-  styleUrls: ['./list-matricula.component.scss']
+  styleUrls: ['./list-matricula.component.scss'],
+  providers: [EscolarService]
 })
 export class ListMatriculaComponent implements OnInit {
 
-  matriculas: Array<Matricula> = [MATRICULA1, MATRICULA2]
-  aluno: Aluno = ALUNO
-  idRota: string
-  constructor(private route: ActivatedRoute) { }
+  private matriculas: Array<Matricula>
+  private aluno: Aluno
+  private idMatricula: string
+  constructor(
+    private route: ActivatedRoute,
+    private nav: Router, 
+    private service: EscolarService,
+    private toast: ToastrService
+    ) { }
 
   ngOnInit() {
-    this.idRota = this.route.snapshot.params['id']
+    this.idMatricula = this.route.snapshot.params['id']
+
+  }
+
+  listaMatriculas(){
+    this.service.getListaObjetos('matricula')
+    .then(resposta => {
+      this.matriculas = resposta
+    })
+    .catch(resposta => {
+      this.toast.error(resposta.json().error, "Erro")
+      this.nav.navigate(['/home/home'])
+    })
   }
 
 }
